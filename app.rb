1ed1d100c323778bc08d8ead3e16a955e0c906d5
gboydhub <eponickstream@gmail.com>
2019-02-26 14:42:46 -0500

    Contact route, favicon, portfolio layout

--- app.rb
+++ app.rb
@@ -16,6 +16,10 @@ get '/resume' do
   erb :resume
 end
 
+get '/contact' do
+  erb :contact
+end
+
 not_found do
   status 404
   erb :index, locals: {error_message: 'Seems you have loaded an invalid link! (404)'}
