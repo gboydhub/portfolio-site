6e59f834b3b39b0622c7ad78b1d1588731bf21f6
gboydhub <eponickstream@gmail.com>
2021-08-30 23:33:42 -0400

    Base website complete

--- contact.php
+++ contact.php
@@ -1,12 +1,10 @@
-<br>
-<div class="contact-div">
-  <a href="mailto:garyboyd.codes@gmail.com"><img src="/img/email.png"> garyboyd.codes@gmail.com</a>
-</div>
+<br><br>
+<center>
+  <div class="contact-div">
+    <a href="mailto:garyboyd.codes@gmail.com"><img src="/img/email.png"><br>garyboyd.codes@gmail.com</a>
+  </div>
 
-<div class="contact-div"><a href="https://linkedin.com/in/devgaryboyd">
-  <img src="/img/linkedin.png"> linkedIn/devGaryBoyd</a>
-</div>
-
-<div class="contact-div"><a href="GaryBoydResume_scrubbed.pdf">
-  <img src="/img/resume-dl.jpg"> PDF Resume<br> (Personal Info Scrubbed)</a>
-</div>
+  <div class="contact-div"><a href="https://linkedin.com/in/devgaryboyd">
+    <img src="/img/linkedin.png"><br>linkedIn/devGaryBoyd</a>
+  </div>
+</center>
